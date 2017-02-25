6e509b9ab7fa6f457af9cb1507f50bfef01dc219
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-24 23:10:23 -0500

    completed ex19

---
+++ js-ad-structures/basic-js/ex19.js
@@ -0,0 +1,13 @@
+// Ex19: Compound Assignment With Augmented Multiplication
+// Convert the assignments for a, b, and c to use the *= operator.
+
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;

809c6f607d8d928df23692ec39f87fe0701c50d5
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-24 23:09:05 -0500

    ex18 completed

---
+++ js-ad-structures/basic-js/ex18.js
@@ -0,0 +1,13 @@
+// Ex18: Compound Assignment With Augmented Subtraction
+// Convert the assignments for a, b, and c to use the -= operator.
+
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;

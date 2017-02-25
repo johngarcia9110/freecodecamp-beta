dd2c361cf76c14ea6656f268234187b9d11ea616
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-25 11:22:57 -0500

    completed ex23

---
+++ js-ad-structures/basic-js/ex23.js
@@ -0,0 +1,4 @@
+// Ex23: Quoting Strings with Single Quotes
+// Change the provided string from double to single quotes and remove the escaping. Do not change anything else.
+
+var myStr = '<a href="http://www.example.com/" target="_blank">Link</a>';

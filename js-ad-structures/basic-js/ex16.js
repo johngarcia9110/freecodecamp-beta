de5a3c334f755ffc0e25cd8eca8cfe603814d27f
johngarcia9110 <johngarcia9110@gmail.com>
2017-02-24 23:00:49 -0500

    completed ex15 and ex16

---
+++ js-ad-structures/basic-js/ex16.js
@@ -0,0 +1,6 @@
+// Ex16: Finding a Remainder in JavaScript
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+// Only change code below this line
+
+var remainder = 11 % 3;
